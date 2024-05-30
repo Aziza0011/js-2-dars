0d6ef203cd88e200201addccc74092a6b22b7167
Aziza0011 <khakimovaaziza95@gmail.com>
2024-05-30 10:00:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = prompt("String typedagi malumot kiriting");
+let c = +prompt("Number typedagi malumot kiriting");
+let d = Boolean(prompt("Boolean typedagi malumot kiriting"));
+let b = confirm("hamma malumotlarni togri kiritkanizga ishonchingiz komilmi?");
+
+alert(`Bu ${typeof a} - DataType digi ma'lumot \n
+Bu ${typeof c} - DataType digi ma'lumot \n
+Bu ${typeof d} - DataType digi ma'lumot \n
+`);
+
+console.log(typeof a);
+console.log(typeof c);
+console.log(typeof b);
